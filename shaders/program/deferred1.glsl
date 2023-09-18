031f06f2c06f6f3c3bd180b52ba7ce99082e2f6a
Steveplays28 <djspaargaren@outlook.com>
2023-09-19 00:05:32 +0200

    feat: :sparkles: Add DH compatibility config screen
    This new config screen contains a Distant Horizons blending toggle and a slider for the Distant Horizons blending view distance factor.

--- shaders/program/deferred1.glsl
+++ shaders/program/deferred1.glsl
@@ -315,13 +315,15 @@ void main() {
             }
         #endif
 
-        // Distant Horizons blending (SSAO)
-        vec3 localPos = (gbufferModelViewInverse * vec4(viewPos)).xyz;
-        localPos.y = 0;
-        float fragDistance = length(localPos);
-        ssao = mix(ssao, 1.0, smoothstep(0.6 * far, far, fragDistance));
+		#if DISTANT_HORIZONS_BLENDING == 1
+			// Distant Horizons blending (SSAO)
+			vec3 localPos = (gbufferModelViewInverse * vec4(viewPos)).xyz;
+			localPos.y = 0;
+			float fragDistance = length(localPos);
+			ssao = mix(ssao, 1.0, smoothstep(DISTANT_HORIZONS_BLENDING_VIEW_DISTANCE_FACTOR * far, far, fragDistance));
+		#endif
 
-        color.rgb *= ssao;
+		color.rgb *= ssao;
 
         #ifdef PBR_REFLECTIONS
             float skyLightFactor = texture6.b;
